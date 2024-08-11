4908f887acb69d304edf9f07476ef059bd95994d
Shayne Hartford <shaybox@shaybox.com>
2024-08-11 00:17:00 -0400

    0.3.11 - Responsive Table

--- src/route/leaderboard.rs
+++ src/route/leaderboard.rs
@@ -24,6 +24,7 @@ pub async fn leaderboard(
     audits: &State<Mutex<AuditLogs>>,
     vrchat: &State<AuthenticatedVRC>,
 ) -> Result<Markup, BadRequest<String>> {
+    /* Keep fetching logs until we find logs we already have saved */
     let logs = {
         let mut audits = audits.lock().await;
         let mut query = GroupAuditLogs {
@@ -58,6 +59,7 @@ pub async fn leaderboard(
         audits.0.clone()
     };
 
+    /* Merge actors and count total actions */
     let mut all_logs_by_actor_id = IndexMap::new();
     for log in logs {
         match log.event_type.as_ref() {
@@ -101,11 +103,13 @@ pub async fn leaderboard(
         }
     }
 
+    /* List new logs within the last 24 hours */
     let mut new_logs_by_actor_id = all_logs_by_actor_id.clone();
     for logs in new_logs_by_actor_id.values_mut() {
         logs.retain(|log| (OffsetDateTime::now_utc() - log.created_at).whole_hours() <= 24);
     }
 
+    /* Sort the leaderboard */
     all_logs_by_actor_id.sort_by(|_, logs1, _, logs2| logs2.len().cmp(&logs1.len()));
 
     #[allow(clippy::cast_possible_truncation)]
@@ -133,99 +137,97 @@ pub async fn leaderboard(
 
             body class="text-center" style="font-family: Dosis" {
                 header class="position-absolute top-0 start-50 translate-middle-x" {
-                    h1 { "Stoner Booth" }
-                    h2 { "Staff Leaderboard" }
+                    h1 { "Staff Leaderboard" }
                 }
 
                 main class="position-absolute top-50 start-50 translate-middle" {
                     a href="https://discord.stonerbooth.com" { "Got banned, aren't a child, and want a second chance? Join the Discord" }
 
-                    table class="table table-striped table-bordered" {
-                        thead {
-                            tr {
-                                th scope="col" { "#" }
-                                th scope="col" { "%" }
-                                th scope="col" { "Display Name" }
-                                th scope="col" { "Bans" }
-                                th scope="col" { "24h" }
-                                th scope="col" { "Kicks" }
-                                th scope="col" { "24h" }
-                                th scope="col" { "Warns" }
-                                th scope="col" { "24h" }
-                                th scope="col" { "Total" }
-                            }
-                        }
-
-                        tbody class="table-group-divider" {
-                            @let [ // What did I just write...
-                                [all_bans, all_kicks, all_warns],
-                                [new_bans, new_kicks, new_warns]
-                            ] = [&all_logs_by_actor_id, &new_logs_by_actor_id].map(|logs| {
-                                [
-                                    "group.user.ban",
-                                    "group.instance.kick",
-                                    "group.instance.warn"
-                                ].map(|event_type| logs.values().flatten().filter(|log| log.event_type == event_type).count() as u32)
-                            });
-                            @let total_total = all_bans + all_kicks + all_warns;
-                            @for (i, (actor_id, logs)) in all_logs_by_actor_id.into_iter().enumerate() {
-                                @let name = vrchat.query(User{ id: actor_id.clone() }).await.map_err(crate::bad_request).map_or_else(|_| actor_id.to_string(), |user| user.as_user().base.display_name.clone());
-                                @let [bans, kicks, warns] = [
-                                    "group.user.ban",
-                                    "group.instance.kick",
-                                    "group.instance.warn"
-                                ].map(|event_type| logs.iter().filter(|log| log.event_type == event_type).count() as u32);
-                                @let [new_bans, new_kicks, new_warns] = [
-                                    "group.user.ban",
-                                    "group.instance.kick",
-                                    "group.instance.warn"
-                                ].map(|event_type| new_logs_by_actor_id.get(&actor_id).map_or(0, |logs| logs.iter().filter(|log| log.event_type == event_type).count()));
-                                @let total = bans + kicks + warns;
-                                @let percent = (f64::from(total - warns) / f64::from(total_total - all_warns)) * 100.0;
-                                @let style = match i {
-                                    0 => "color: #d6af36; font-weight: bold",
-                                    1 => "color: #a77044; font-weight: bold",
-                                    2 => "color: #a7a7ad; font-weight: bold",
-                                    _ => "color: grey",
-                                };
-
+                    div class="table-responsive" style="max-height: 88vh" {
+                        table class="table table-striped table-bordered" {
+                            thead {
                                 tr {
-                                    th style=(style) scope="row" { (i + 1) }
-                                    td style=(style) { (format!("{percent:.1}")) }
-                                    td style=(style) { (name) }
-                                    td style=(style) { (bans) }
-                                    td style=(style) { (new_bans) }
-                                    td style=(style) { (kicks) }
-                                    td style=(style) { (new_kicks) }
-                                    td style=(style) { (warns) }
-                                    td style=(style) { (new_warns) }
-                                    td style=(style) { (total) }
+                                    th scope="col" { "#" }
+                                    th scope="col" { "%" }
+                                    th scope="col" { "Display Name" }
+                                    th scope="col" { "Bans" }
+                                    th scope="col" { "24h" }
+                                    th scope="col" { "Kicks" }
+                                    th scope="col" { "24h" }
+                                    th scope="col" { "Warns" }
+                                    th scope="col" { "24h" }
+                                    th scope="col" { "Total" }
+                                }
+                            }
+
+                            tbody class="table-group-divider" {
+                                @let [ // What did I just write...
+                                    [all_bans, all_kicks, all_warns],
+                                    [new_bans, new_kicks, new_warns]
+                                ] = [&all_logs_by_actor_id, &new_logs_by_actor_id].map(|logs| {
+                                    [
+                                        "group.user.ban",
+                                        "group.instance.kick",
+                                        "group.instance.warn"
+                                    ].map(|event_type| logs.values().flatten().filter(|log| log.event_type == event_type).count() as u32)
+                                });
+                                @let total_total = all_bans + all_kicks + all_warns;
+                                @for (i, (actor_id, logs)) in all_logs_by_actor_id.into_iter().enumerate() {
+                                    @let name = vrchat.query(User{ id: actor_id.clone() }).await.map_err(crate::bad_request).map_or_else(|_| actor_id.to_string(), |user| user.as_user().base.display_name.clone());
+                                    @let [bans, kicks, warns] = [
+                                        "group.user.ban",
+                                        "group.instance.kick",
+                                        "group.instance.warn"
+                                    ].map(|event_type| logs.iter().filter(|log| log.event_type == event_type).count() as u32);
+                                    @let [new_bans, new_kicks, new_warns] = [
+                                        "group.user.ban",
+                                        "group.instance.kick",
+                                        "group.instance.warn"
+                                    ].map(|event_type| new_logs_by_actor_id.get(&actor_id).map_or(0, |logs| logs.iter().filter(|log| log.event_type == event_type).count()));
+                                    @let total = bans + kicks + warns;
+                                    @let percent = (f64::from(total - warns) / f64::from(total_total - all_warns)) * 100.0;
+                                    @let style = match i {
+                                        0 => "color: #d6af36; font-weight: bold",
+                                        1 => "color: #a77044; font-weight: bold",
+                                        2 => "color: #a7a7ad; font-weight: bold",
+                                        _ => "color: grey",
+                                    };
+
+                                    tr {
+                                        th style=(style) scope="row" { (i + 1) }
+                                        td style=(style) { (format!("{percent:.1}")) }
+                                        td style=(style) { (name) }
+                                        td style=(style) { (bans) }
+                                        td style=(style) { (new_bans) }
+                                        td style=(style) { (kicks) }
+                                        td style=(style) { (new_kicks) }
+                                        td style=(style) { (warns) }
+                                        td style=(style) { (new_warns) }
+                                        td style=(style) { (total) }
+                                    }
+                                }
+                            }
+
+                            tbody class="table-group-divider" {
+                                tr {
+                                    th scope="row" { "#" }
+                                    td { "100.0" }
+                                    td { "Total" }
+                                    td { (all_bans) }
+                                    td { (new_bans) }
+                                    td { (all_kicks) }
+                                    td { (new_kicks) }
+                                    td { (all_warns) }
+                                    td { (new_warns) }
+                                    td { (total_total) }
                                 }
                             }
                         }
-
-                        tbody class="table-group-divider" {
-                            tr {
-                                th scope="row" { "#" }
-                                td { "100.0" }
-                                td { "Total" }
-                                td { (all_bans) }
-                                td { (new_bans) }
-                                td { (all_kicks) }
-                                td { (new_kicks) }
-                                td { (all_warns) }
-                                td { (new_warns) }
-                                td { (total_total) }
-                            }
-                        }
                     }
-
-                    p id="last" { (Utc::now().to_rfc3339()) }
-                    p id="next" { "Updates every 4 hours" }
                 }
 
                 footer class="position-absolute bottom-0 start-50 translate-middle-x" {
-                    p data-bs-toggle="tooltip" data-bs-placement="top" title="Queers" { "Cheers!" }
+                    p data-bs-toggle="tooltip" data-bs-placement="top" title="Updates every four hours" id="last" { (Utc::now().to_rfc3339()) }
                 }
 
                 script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" {}
@@ -244,8 +246,7 @@ pub async fn leaderboard(
                         const nextMins = Math.round(((nextMs % 86400000) % 3600000) / 60000);
                         const nextHours = nextHrs > 0 ? `${nextHrs}h ` : '';
 
-                        last.innerHTML = `Last Update ${lastHours}${lastMins}m ago`;
-                        next.innerHTML = `Next Update ${nextHours}${nextMins}m`;
+                        last.innerHTML = `Last Update ${lastHours}${lastMins}m ago  |  Next Update in ${nextHours}${nextMins}m`;
 
                         if (nextMs <= 0) {
                             location.reload();
